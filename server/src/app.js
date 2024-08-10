fcde2b9d9f8e7e8532f0447056a2dccbb573bba9
Sandesh225 <86834922+Sandesh225@users.noreply.github.com>
2024-08-10 13:35:54 +0545

    Refactor app.js to enhance application setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,24 @@
 
 import express from "express"
+import cookieParser from "cookie-parser";
+
+
 import { errorHandler } from "./middlewares/errorHandler.js";
+import userRoutes from "./routes/user.routes.js"
+
+
+
+
 
 const app=express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+
+
+app.use("/users",userRoutes)
 
 app.use(errorHandler)
 
