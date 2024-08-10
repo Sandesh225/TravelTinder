c7442bc57b5291eccbe6538c063807a3a44052b0
Sandesh225 <86834922+Sandesh225@users.noreply.github.com>
2024-08-10 13:36:35 +0545

    Enhance asyncHandler.js for better async error handling

--- server/src/middlewares/asyncHandler.js
+++ server/src/middlewares/asyncHandler.js
@@ -1,5 +1,5 @@
-const asyncHandler = fn => (req, res, next) => {
+// middlewares/asyncHandler.js
+const asyncHandler = (fn) => (req, res, next) =>
   Promise.resolve(fn(req, res, next)).catch(next);
-};
 
-export {asyncHandler}
+export { asyncHandler };
