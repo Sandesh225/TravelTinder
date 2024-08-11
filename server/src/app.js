059539dcf7daa58a5d7303d1e1046e4cc1c53acb
Sandesh225 <86834922+Sandesh225@users.noreply.github.com>
2024-08-11 21:28:47 +0545

    Updated server/app.js for better integration with frontend changes

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,10 @@ import cookieParser from "cookie-parser";
 
 import userRoutes from "./routes/user.routes.js"
 import profileRoutes from './routes/profile.routes.js';
-
+import travelRoutes from './routes/travelPrefences.routes.js'
+import matchRoutes from './routes/match.routes.js'
+import notificationRoutes from './routes/notification.routes.js'
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 
 
@@ -18,9 +21,12 @@ app.use(cookieParser());
 
 
 
-app.use("/users",userRoutes)
-app.use("/profile",profileRoutes)
+app.use("/api/users",userRoutes)
+app.use("/api/profile",profileRoutes)
+app.use("/api/travel",travelRoutes)
+app.use("/api/matches",matchRoutes)
+app.use("/api/notifications",notificationRoutes)
 
-
+app.use(errorHandler)
 
 export {app}
