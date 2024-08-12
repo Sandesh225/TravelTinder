49c3e40d2a5b067814d39f7eaf511caa9b0a3fc7
Sandesh225 <86834922+Sandesh225@users.noreply.github.com>
2024-08-12 19:28:27 +0545

    Refactor App component for improved performance

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,32 +2,32 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import "./index.css";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
+import Footer from "./components/Footer";
 import UserList from "./UserList/UserList";
 import Logout from "./pages/Logout/Logout";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
-import UserForm from "./UserList/UserForm";
 import ProfileSetup from "./Profile/ProfileSetup";
-const App = () => {
-  return (
-    <Router>
-      <Navbar />
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
+
+const App = () => (
+  <Router>
+    <Navbar />
+    <main className="bg-background-light min-h-screen">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/form" element={<UserForm />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/Login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/users" element={<UserList />} />
         <Route path="/profile" element={<ProfileSetup />} />
       </Routes>
-      <ToastContainer position="top-center" />
-    </Router>
-  );
-};
+    </main>
+    <Footer />
+    <ToastContainer position="top-center" />
+  </Router>
+);
 
 export default App;
