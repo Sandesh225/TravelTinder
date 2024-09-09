bd3e849d74ae53fd0aa0e19b0d5e099e5046e74c
Sandesh225 <86834922+Sandesh225@users.noreply.github.com>
2024-09-09 22:01:08 +0545

    Add middleware to handle 404 errors and send appropriate responses

---
+++ server/src/middlewares/notFoundHandler.js
@@ -0,0 +1,8 @@
+// Middleware to handle 404 errors
+const notFoundHandler = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+export { notFoundHandler };
