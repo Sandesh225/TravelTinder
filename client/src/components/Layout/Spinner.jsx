db4dd906de1a259e5e6188608ed0b0621260f5ac
Sandesh225 <86834922+Sandesh225@users.noreply.github.com>
2024-09-25 20:34:29 +0545

    refactor(spinner): optimize Spinner component for better loading feedback

--- client/src/components/Layout/Spinner.jsx
+++ client/src/components/Layout/Spinner.jsx
@@ -1,11 +1,11 @@
-// src/components/Layout/Spinner.jsx
 import React from "react";
 
-const Spinner = () => (
-  <div className="flex items-center justify-center h-screen">
-    <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
-    <p>Loading...</p>
-  </div>
-);
+const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32"></div>
+    </div>
+  );
+};
 
 export default Spinner;
