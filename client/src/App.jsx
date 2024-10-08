1f63047fa9e659ba2fa0f63ec3e57d6603bda16f
Sandesh225 <86834922+Sandesh225@users.noreply.github.com>
2024-10-08 22:15:14 +0545

    Modify App.jsx for improved component structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,111 +1,43 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import {
-  setCredentials,
-  getUserFromLocalStorage,
-  logOut,
-} from "./features/authSlice";
-import Navbar from "./components/Navbar/Navbar";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./components/Home";
+import HeroSection from "./components/Hero/HeroSection";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import SwipeableCard from "./components/TravelCard/SwipeableCard/SwipeableCard";
+import TravelPreferencesForm from "./components/Travel/TravelPreferencesForm";
+import UserTravelPreferences from "./components/Travel/UserTravelPreferences";
+import UpdateTravelPreferencesForm from "./components/Travel/UpdateTravelPreferencesForm";
+import ProfileCard from "./components/Profile/ProfileCard";
+import ProfileList from "./components/Profile/ProfileList";
+import ProfileSetup from "./components/Profile/ProfileSetup";
 
-// Lazy load components
-const Hero = React.lazy(() => import("./components/Hero/Hero"));
-const Home = React.lazy(() => import("./components/Home/Home"));
-const ProfileSetup = React.lazy(() =>
-  import("./components/Profile/ProfileSetup")
-);
-const Profile = React.lazy(() => import("./components/Profile/Profile"));
-const Login = React.lazy(() => import("./pages/Login/Login"));
-const Register = React.lazy(() => import("./pages/Register/Register"));
-const Explore = React.lazy(() => import("./components/Explore/Explore"));
-const MatchesPage = React.lazy(() => import("./components/Match/MatchesPage"));
-import ProfileDetail from "./components/Profile/ProfileDetail";
-import ProtectedRoute from "./components/ProtectedRoute";
-
-function App() {
-  const dispatch = useDispatch();
-  const token = useSelector((state) => state.auth.token);
-
-  useEffect(() => {
-    const storedUser = getUserFromLocalStorage();
-    if (storedUser) {
-      dispatch(setCredentials({ user: storedUser }));
-    }
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (token) {
-      const expirationTime = getTokenExpiration(token); // Implement this function to decode token expiration
-      const currentTime = Date.now();
-
-      if (expirationTime < currentTime) {
-        dispatch(logOut());
-      }
-    }
-  }, [token, dispatch]);
-
+const App = () => {
   return (
     <Router>
       <Navbar />
-      <React.Suspense fallback={<div className="spinner">Loading...</div>}>
-        <Routes>
-          {/* Public Routes */}
-          <Route path="/" element={<Hero />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+      <Routes>
+        <Route path="/signup" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<HeroSection />} />
+        <Route path="/explore" element={<SwipeableCard />} />
+        <Route path="/travel-form" element={<TravelPreferencesForm />} />
+        <Route path="/travel-profile" element={<UserTravelPreferences />} />
+        <Route
+          path="/update-travel"
+          element={<UpdateTravelPreferencesForm />}
+        />
+        <Route path="/card" element={<ProfileSetup />} />
+      </Routes>
 
-          {/* Protected Routes */}
-          <Route
-            path="/profile-setup"
-            element={
-              <ProtectedRoute>
-                <ProfileSetup />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/profiles/:id"
-            element={
-              <ProtectedRoute>
-                <ProfileDetail />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/matches"
-            element={
-              <ProtectedRoute>
-                <MatchesPage />
-              </ProtectedRoute>
-            }
-          />
-
-          {/* Explore Page */}
-          <Route path="/explore" element={<Explore />} />
-
-          {/* Catch-all route */}
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </React.Suspense>
-      <ToastContainer position="top-center" autoClose={5000} hideProgressBar />
+      <ToastContainer position="top-right" autoClose={5000} />
     </Router>
   );
-}
+};
 
 export default App;
