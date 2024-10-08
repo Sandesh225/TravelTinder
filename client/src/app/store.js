9e37b0708fbdb55145f1a5d80c01cf909146402f
Sandesh225 <86834922+Sandesh225@users.noreply.github.com>
2024-10-08 22:15:54 +0545

    Update store configuration with new Redux setup

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,14 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { userApiSlice } from '../features/apiSlice'
-import authReducer from '../features/authSlice'
-
+import { apiSlice } from './features/apiSlice';
+import authReducer from './features/authSlice'
 export const store = configureStore({
   reducer: {
-    [userApiSlice.reducerPath]: userApiSlice.reducer,  // Register API slice
-    auth: authReducer,  // Register auth slice
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(userApiSlice.middleware),  // Add API middleware
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export default store;
