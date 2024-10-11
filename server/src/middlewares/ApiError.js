ee32ea827d0b41be51d3f688180b5450e75f954d
Sandesh225 <86834922+Sandesh225@users.noreply.github.com>
2024-10-11 22:36:57 +0545

    Refactor ApiError middleware for better error handling

--- server/src/middlewares/ApiError.js
+++ server/src/middlewares/ApiError.js
@@ -1,23 +1,19 @@
-// middlewares/ApiError.js
 class ApiError extends Error {
-    constructor(statusCode, message, isOperational = true, stack = "",errors=[]) {
-      super(message);
-      this.statusCode = statusCode;
-      this.isOperational = isOperational;
-      this.message = message;
-      this.data = null
-      this.errors = errors
-      this.success = false;
-  
-      if (stack) {
-        this.stack = stack;
-      } else {
-        Error.captureStackTrace(this, this.constructor);
-      }
+  constructor(statusCode, message = "An error occurred", isOperational = true, stack = "", errors = []) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+    this.message = message;
+    this.data = null;
+    this.errors = errors;
+    this.success = false;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
     }
   }
-  
-  export { ApiError };
-  
-  
-  
+}
+
+export { ApiError };
