db7069786f2a586a1bff72a920468eee1bd62bc5
Sandesh225 <86834922+Sandesh225@users.noreply.github.com>
2024-10-19 21:31:56 +0545

    Updated ApiResponse middleware: enhanced response formatting and included support for more status codes.

--- server/src/middlewares/ApiResponse.js
+++ server/src/middlewares/ApiResponse.js
@@ -1,10 +1,10 @@
 class ApiResponse {
-  constructor(statusCode, data, message = "Success") {
+  constructor(statusCode, data, message = "Success", errors = []) {
     this.statusCode = statusCode;
     this.data = data;
     this.message = message;
+    this.errors = errors;
     this.success = statusCode < 400;
   }
 }
-
-export { ApiResponse };
+export {ApiResponse}
